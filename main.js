62614787097fe62ab95173b43ac49a85095f652b
MrOverzealous <overzealouspixels@gmail.com>
2025-03-18 21:44:57 -0400

    Finished the basic version of the dungeon selection

--- main.js
+++ main.js
@@ -157,7 +157,6 @@ function newTeam() {
         displayTeam(team);
     });
 }
-
 function displayTeam(team) {
     Object.keys(team).forEach((key, index) => {
         const sectionId = `member-${textNums[index].toLowerCase()}`;
@@ -195,7 +194,6 @@ function displayHeroData(hero, sectionId) {
         stats.appendChild(item);
     }
 }
-
 function handleHeroButtons(container) {
 
     container.innerText = '';
@@ -236,12 +234,99 @@ function handleHeroButtons(container) {
 }
 
 
+
+
+
+class Dungeon {
+    constructor(name, levelReq, opponents) {
+        this.name = name;
+        this.levelReq = levelReq;
+        this.opponents = opponents;
+    }
+}
+class Enemy {
+    constructor(name, type, baseExp, stats, loot, lootMult) {
+        this.name = name;
+        this.type = type;
+        this.baseExp = baseExp;
+        this.stats = stats;
+        this.loot = loot;
+        this.lootMult = lootMult;
+    }
+}
+
+const goblin = new Enemy("Goblin", "monster", 5, { hp: 5, atk: 1, mag: 1, spd: 1, def: 1, res: 1 }, [], 1);
+const forestDungeon = new Dungeon("Forest Dungeon", 1, [ goblin ]);
+const availableDungeons = [ forestDungeon ];
+
+function handleAvailableDungeons() {
+    let maxLvl = 0;
+    for (let i = 0; i < heroes.length; i++) {
+        if (heroes[i].level > maxLvl) {
+            maxLvl = heroes[i].level;
+        }
+    }
+    // add dungeon to unlocked list if unlocked > button
+}
+
+function displayDungeons() {
+    handleAvailableDungeons();
+    const container = document.querySelector('#assignment-options');
+    const buttons = container.querySelectorAll('button');
+    // redraw buttons if needed
+    if (buttons.length !== availableDungeons.length) {
+        container.innerText = '';
+        for (let i = 0; i < availableDungeons.length; i++) {
+            const dungeon = availableDungeons[i];
+            const button = document.createElement('button');
+            button.innerText = `${dungeon.name} Lvl: ${dungeon.levelReq}`;
+            button.addEventListener('click', () => {
+                const prevSelected = container.querySelector('.selected');
+                if (prevSelected !== null) {
+                    prevSelected.classList.remove('selected');
+                }
+                button.classList.add('selected');
+
+                const details = document.querySelector('#assignment-details');
+                details.innerText = '';
+
+                const heading = document.createElement('h5');
+                heading.innerText = button.innerText;
+                details.appendChild(heading)
+
+                for (let j = 0; j < dungeon.opponents.length; j++) {
+                    const item = document.createElement('li');
+                    item.innerText = `${dungeon.opponents[j].name}`;
+                    details.appendChild(item);
+                }
+            });
+            container.appendChild(button);
+        }
+    }
+}
+// add a remove button to each hero slot 
+// need to add a 'begin' button
+// if both a team and dungeon are .selected, both listeners will need to check for that condition, and update the button class/color
+// if a team is currently active, then other teams with them on it need that to be displayed and stopped from being able to send that team out unless it only has members that aren't active
+// begin should update the Activity section, and summarize what's going on in a slot
+// (unsure if it should be updated frequently or load updates when Explore is clicked)
+// begin should close the current modal and either open a new one, or display it on the main screen
+// regardless, it should display each characters Name and HP, and a box for each enemy
+// obv need to generate a combat sequence where enemies are generated and displayed
+// then need to work on them interacting with one another, being able to select attack, skills, defend, etc
+// then need to automate it so that they can auto-battle
+// there's currently no resource (like mana) attached to the classes
+// no currentClass prop on heroes either
+// prob want to extract classes into their own thing so they can be assigned, eventually skills as well
+// then it can be reused for enemies as well
+
 function updateScreen() {
 
 }
 
 document.querySelector('#explore').addEventListener('click', () => {
     document.querySelector('#explore-modal').showModal();
+    displayDungeons();
     if (teams.length > 0) {
 
     }
